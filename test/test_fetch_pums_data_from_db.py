b6e232364e4144feac7139f1ae58be506d5afe92
Nikki Six <nicolai.tesela@gmail.com>
2017-09-01 08:14:46 -0500

    fixing fetch data from db test

--- test/test_fetch_pums_data_from_db.py
+++ test/test_fetch_pums_data_from_db.py
@@ -3,11 +3,45 @@ from __future__ import (
 )
 import unittest
 import mock
+import os
 from doppelganger.scripts.fetch_pums_data_from_db import link_fields_to_inputs, fetch_pums_data
 from doppelganger import inputs, config
 
 
 class TestFetchPumsDataFromDB(unittest.TestCase):
+
+    def _mock_config(self):
+        conf = {
+            "person_fields": [
+                "age",
+                "sex",
+                "individual_income"
+            ],
+            "household_fields": [
+                "num_people",
+                "household_income",
+                "num_vehicles"
+            ],
+            "preprocessing": {
+                "individual_income": {
+                    "bins": [
+                        0,
+                        20000,
+                        40000,
+                        80000,
+                        100000
+                    ]
+                }
+            },
+            "network_config_files": {
+                "person": os.path.dirname(__file__)
+                + "/../examples/sample_data/sample_person_bn.json",
+                "household": os.path.dirname(__file__)
+                + "/../examples/sample_data/sample_household_bn.json"
+            },
+            "version": "0"
+        }
+        return config.Configuration.from_json(conf)
 
     def _mock_legit_person_input_list(self):
         return ['age', 'sex', 'individual_income']
@@ -23,7 +57,7 @@ class TestFetchPumsDataFromDB(unittest.TestCase):
     @mock.patch('doppelganger.scripts.fetch_pums_data_from_db.psycopg2')
     @mock.patch('doppelganger.scripts.fetch_pums_data_from_db.datasource.PumsData')
     def test_fetch_pums_data(self, mock_PumsData, mock_psycopg2):
-        mock_config = config.Configuration.from_json(self._mock_config())
+        mock_config = self._mock_config()
         persons_data, households_data =\
             fetch_pums_data(state_id='01', puma_id='00001', configuration=mock_config,
                             db_host='host1', db_database='db1', db_schema='schema1',
